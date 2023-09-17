bb9e16e744396b961ce7bcbc6f1d9dbb79d9eb3c
Dmitrii <dtihonov982@gmail.com>
2023-09-17 08:47:22 +0300

    Release options

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 mkdir build
-cmake -B build -S . &&
+cmake -B build -S . -DCMAKE_BUILD_TYPE=Release &&
 cmake --build build
---
+++ prof/prof.sh
@@ -0,0 +1,1 @@
+valgrind --tool=callgrind --dump-instr=yes --collect-jumps=yes ../build/programs/route ../tests/input10000.json ../data/cells0.json output10000.json
