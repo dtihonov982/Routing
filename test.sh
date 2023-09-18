8c818e123269368cd374ef8ee49618aa071fa4e8
Dmitrii <dtihonov982@gmail.com>
2023-09-19 00:12:07 +0300

    Change test.sh

--- test.sh
+++ test.sh
@@ -1,2 +1,20 @@
-build/programs/route data/input2.json data/cells2.json output2.json &&
-tests/draw.py data/cells2.json output2.json
+#!/bin/bash
+
+if [ $# -eq 0 ]
+then
+    INPUT=data/input2.json
+    CELLS=data/cells2.json
+    OUTPUT=data/output2.json
+elif [ $# -eq 3 ]
+then
+    INPUT=$1
+    CELLS=$2
+    OUTPUT=$3
+else
+    echo "Usage: test.sh input_json cells_json output_json"
+    exit 1
+fi
+
+
+build/programs/route $INPUT $CELLS $OUTPUT &&
+tests/draw.py $CELLS $OUTPUT
