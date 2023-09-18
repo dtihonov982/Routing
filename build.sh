5b10ccc49ce194b6656c562f3655c660c53fce23
Dmitrii <dtihonov982@gmail.com>
2023-09-18 05:53:16 +0300

    Optional build

--- build.sh
+++ build.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
-mkdir build
-cmake -B build -S . -DCMAKE_BUILD_TYPE=Release &&
-cmake --build build -j 12
+test ! -d build && mkdir build
+
+if [ $# -eq 0 ]
+then
+    cmake -B build -S . -DCMAKE_BUILD_TYPE=Release
+elif [ $1 = debug ] 
+then 
+    cmake -B build -S . -DCMAKE_BUILD_TYPE=Debug -DUSE_ASAN=OFF
+elif [ $1 = asan ] 
+then 
+    cmake -B build -S . -DCMAKE_BUILD_TYPE=Debug -DUSE_ASAN=ON
+else
+    echo "Unrecognized option $1"
+    exit 1
+fi
+
+if [ $? -eq 0 ]
+then
+    cmake --build build -j 12
+fi
